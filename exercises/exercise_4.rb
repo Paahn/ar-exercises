630117248e8c69b7d180ff9bf197b6fc4e676548
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-02-05 20:04:51 +0000

    outputting the name and annual revenue for the stores with mens apparel

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,13 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store4 = Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true)
+store5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true)
+store6 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+store4.save
+store5.save
+store6.save
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |s|  
+  puts "#{s.name} has annual revenue #{s.annual_revenue}"
+end
