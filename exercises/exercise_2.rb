04e8e15550f1767c312d27bdb81cb4280f9813f0
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-02-05 19:37:40 +0000

    renamed the first store to Tsawassen

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+@store1.name = "Tsawassen"
+@store1.save
+
+puts "Now the first store is named #{@store1.name}"
