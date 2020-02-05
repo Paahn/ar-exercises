3437ebf9de5241eae0ce525a241b3e34987c00ce
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-02-05 21:45:20 +0000

    added a bunch of employees to 2 stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_rev = Store.sum("annual_revenue")
+puts "This is the total revenue for all stores: #{total_rev}"
+puts "The average revenue for a store is #{total_rev/Store.count}"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,19 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store
+  has_many :employees
+end
+
+class Employee
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jo", last_name: "Lent", hourly_rate: 60)
+@store1.employees.create(first_name: "Milena", last_name: "Kelos", hourly_rate: 60)
+@store2.employees.create(first_name: "Kratos", last_name: "Pangaios", hourly_rate: 60)
+@store2.employees.create(first_name: "Falker", last_name: "Guy", hourly_rate: 60)
+@store2.employees.create(first_name: "Nym", last_name: "Poliknish", hourly_rate: 60)
+@store2.employees.create(first_name: "Korina", last_name: "Onassi", hourly_rate: 60)
+
